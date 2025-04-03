66552548abfb8e888ffc7785be9ef3aa34d2a452
Joaorx7 <joaogabriel191820@gmail.com>
2025-04-02 21:41:14 -0300

    versão 7

--- tarefas/static/js/script.js
+++ tarefas/static/js/script.js
@@ -1,1 +1,17 @@
-console.log("Funcionou");
+//função que pergunta ao usuario se ele deseja excluir uma tarefa
+
+document.querySelectorAll(".delete-btn").forEach(
+    btn => {
+        btn.addEventListener("click", function(e){
+            e.preventDefault();
+
+            const delLink = this.getAttribute("href");
+
+            if(delLink && confirm("Quer deletar essa tarefa?"))
+            {
+                window.location.href = delLink;
+            }
+        });
+    }
+
+)
--- tarefas/views.py
+++ tarefas/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Tarefas
 from .forms import TarefaForm
+
 # Create your views here.
 
 def listaTarefa(request):
@@ -24,3 +25,25 @@ def novaTarefa(request):
     else:
         form = TarefaForm()
         return render(request, 'tarefas/addTarefa.html', {'form':form})
+    
+
+def editTarefa(request, id):
+    tarefa = get_object_or_404(Tarefas, pk=id)
+    form = TarefaForm(instance=tarefa)
+
+    if(request.method == 'POST'):
+        form = TarefaForm(request.POST, instance=tarefa)
+
+        if(form.is_valid()):
+            tarefa.save()
+            return redirect('/')
+        else:
+            return render(request, 'tarefas/editTarefa.html', {'form':form, 'tarefa':tarefa})
+    
+    else:
+        return render(request, 'tarefas/editTarefa.html', {'form':form, 'tarefa':tarefa})
+
+def deleteTarefa(request, id):
+    tarefa = get_object_or_404(Tarefas, pk=id)
+    tarefa.delete()
+    return redirect('/')
